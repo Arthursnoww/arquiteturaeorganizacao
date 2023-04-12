42eb86a215950613b1a71bb00af23bf7fd26fac5
Arthur Roberto <111710917+Arthursnoww@users.noreply.github.com>
2023-04-12 12:55:40 -0300

    Update QUESTAO_6.asm

--- ATIVIDADES DOS CAPITULOS/CAP_3/QUESTAO_6.asm
+++ ATIVIDADES DOS CAPITULOS/CAP_3/QUESTAO_6.asm
@@ -3,21 +3,21 @@
         .stack 100h
 printf 	PROTO arg1:Ptr Byte, printlist:VARARG
 scanf 	PROTO arg2: Ptr byte, inputlist:VARARG
-	      .data
+	.data
 ini1fmt byte "%d",0
 msg1fmt byte 0Ah, "%s",0
-msg1 	  byte "Enter the number of amperes: ",0
+msg1 	byte "Enter the number of amperes: ",0
 msg2fmt byte 0Ah, "%s",0
-msg2 	  byte "Enter the number of ohms: ",0
+msg2 	byte "Enter the number of ohms: ",0
 msg3fmt byte 0Ah,"%s%d",0Ah,0Ah,0
-msg3 	  byte "The number of volts is: ",0
+msg3 	byte "The number of volts is: ",0
 msg4fmt byte 0Ah,"%s%d",0Ah,0Ah,0
-msg4 	  byte "The number of watts is: ",0
+msg4 	byte "The number of watts is: ",0
 amperes sdword ?
-ohms 	  sdword ?
+ohms 	sdword ?
 watts 	sdword ?
 volts 	sdword ?
-	      .code
+	.code
 main proc
         INVOKE printf, ADDR msg1fmt, ADDR msg1
         INVOKE scanf, ADDR ini1fmt, ADDR amperes
@@ -35,4 +35,4 @@ main proc
         INVOKE printf, ADDR msg4fmt, ADDR msg4, watts
         ret
 main    endp
-	      end
+	end
