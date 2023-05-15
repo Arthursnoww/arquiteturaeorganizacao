cfa7545dff1f77fcb2df38bd7cb42b48ca5d0875
Arthur Roberto <111710917+Arthursnoww@users.noreply.github.com>
2023-05-15 10:14:42 -0300

    Update questao_10_B.asm

--- ATIVIDADES/CAP_5/questao_10_B.asm
+++ ATIVIDADES/CAP_5/questao_10_B.asm
@@ -1,19 +1,19 @@
         .686
         .model flat, c
-	      .stack 100h
-scanf	  PROTO arg2:Ptr Byte, inputlist:VARARG
+	.stack 100h
+scanf	PROTO arg2:Ptr Byte, inputlist:VARARG
 printf	PROTO arg1:Ptr Byte, printlist:VARARG
-		    .data
+	.data
 in1fmt	byte "%d",0
 msg0fmt	byte "%s", 0Ah, 0
 msg1fmt	byte "%d%s%d", 0Ah,0
-msg1	  byte "digite o numero:",0
-msg2	  byte " corresponde ao numero da sequencia Fibonacci ",0
-n	      sdword	?
-a		    sdword  ?
-b		    sdword	1
-	      .code
-  main	proc
+msg1	byte "digite o numero:",0
+msg2	byte " corresponde ao numero da sequencia Fibonacci ",0
+n	sdword	?
+a	sdword  ?
+b	sdword	1
+	.code
+main	proc
         INVOKE printf, ADDR msg0fmt, ADDR msg1
         INVOKE scanf, ADDR in1fmt, ADDR n
         .if n != 0
@@ -31,5 +31,5 @@ b		    sdword	1
         .until ecx > n	
         .endif
         INVOKE printf, ADDR msg1fmt, n, ADDR msg2, b
-main	  endp
-	      end
+main	endp
+	end
